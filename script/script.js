8d7ed90845a7d5744e91d41ca8587132b21b1fb1
Drago1426 <peter.sheehen.c10396@mcast.edu.mt>
2023-04-02 16:31:14 +0200

    Finished home page, added comments, arranged stylesheet

---
+++ script/script.js
@@ -0,0 +1,64 @@
+function clearErrors(){
+    document.getElementById('errors-content').innerHTML = "";
+
+    document.getElementById('errors').classList.remove('d-block');
+
+    document.getElementById('errors').classList.add('d-none');
+}
+
+function validateForm() {
+    
+    clearErrors();
+
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    var errors = "";
+
+    if (isEmpty(firstName) ||
+        isEmpty(lastName) ||
+        isEmpty(email) ||
+        isEmpty(message)) {
+        errors = "<li>Please fill in all fields.</li>";
+    }
+
+    if (checkLength(message)) {
+        errors += "<li>Write more!</li>";
+    }
+
+
+
+    if (errors != "") {
+        document.getElementById('errors-content').innerHTML = "<ul class='errors-list'>" + errors + "</ul>";
+
+        document.getElementById('errors').classList.remove('d-none');
+
+        document.getElementById('errors').classList.add('d-block');
+
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+function isEmpty(field) {
+    if (field == "") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function checkLength(field) {
+    if (field.length < 20) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
